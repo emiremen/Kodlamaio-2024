54e88a0fc07a084e16ce57c3a7f122d2d47f968c
Muhammed EMEN <emiremen001@gmail.com>
2024-02-09 14:54:40 +0300

    Initial Commit

---
+++ Odev-1-MaskeTakip/Business/Concrete/PersonManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PersonManager : IApplicantService
+{
+    public void ApplyForMask(Person person)
+    {
+        if (CheckPerson(person))
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName} için maske başvurusu yapıldı.");
+        }
+        else
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName} için maske başvurusu yapılamadı!");
+        }
+    }
+
+    public List<Person> GetList()
+    {
+        return null;
+    }
+
+    //https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx
+    public bool CheckPerson(Person person)
+    {
+        KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+        return client.TCKimlikNoDogrulaAsync(
+            new TCKimlikNoDogrulaRequest(
+                new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthYear))).Result.Body.TCKimlikNoDogrulaResult;
+    }
+}
---
+++ Odev-1-MaskeTakip/Workspace/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+
+
+
+Person person1 = new Person();
+
+person1.FirstName = "Ali";
+person1.LastName = "Bey";
+person1.NationalIdentity = 12345678910;
+person1.DateOfBirthYear = 1999;
+
+Console.WriteLine("First Name: \t" + person1.FirstName);
+Console.WriteLine("Last Name: \t" + person1.LastName);
+Console.WriteLine("Birth Year: \t" + person1.NationalIdentity);
+Console.WriteLine("TC No: \t\t" + person1.DateOfBirthYear);
+
+
+PersonManager personManager = new PersonManager();
+personManager.ApplyForMask(person1);
+
+Console.WriteLine("\n#####\n");
+
+Person person2 = new Person();
+person2.FirstName = "Veli";
+person2.LastName = "Bey";
+person2.NationalIdentity = 10987654321;
+person2.DateOfBirthYear = 1999;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person2);
